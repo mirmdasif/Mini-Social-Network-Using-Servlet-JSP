5cc03139081d25fca5e4e98105e4d9fa404c6ffc
Mir Md. Asif Hossain <asif.hossain@asif.hossain>
2014-10-16 10:01:20 +0600

    Basic cleanups

--- src/com/hossain/asif/main/dao/FollowerDao.java
+++
@@ -1,9 +0,0 @@
-package com.hossain.asif.main.dao;
-
-/**
- * Created by sadasidha on 10/13/14.
- */
-public interface FollowerDao {
-    void follow(int userID, int followerId);
-    void unFollow(int userId, int followerId);
-}
